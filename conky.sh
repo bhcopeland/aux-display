936b0512777e50163af265e2052e84492961a7c5
Benjamin Copeland <ben.copeland@linaro.org>
2024-11-29 08:49:30 +0000

    Updates to conky to use nvidia and changes to cpu
    Signed-off-by: Benjamin Copeland <ben.copeland@linaro.org>

--- conky.sh
+++ conky.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sleep 5
-conky -c ~/Documents/conkyl.conf &
-conky -c ~/Documents/conkylm.conf &
-conky -c ~/Documents/conkyrm.conf &
-conky -c ~/Documents/conkyr.conf &
+conky -c "${PWD}/conkyl.conf" &
+conky -c "${PWD}/conkylm.conf" &
+conky -c "${PWD}/conkyrm.conf" &
+conky -c "${PWD}/conkyr.conf" &
+
